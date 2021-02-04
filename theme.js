fcec8385d6a054052bbe5d790a71fac1f6e50f68
Felipe Lobo <felipeblobo@gmail.com>
2021-02-03 22:29:55 -0300

    dark mode

---
+++ theme.js
@@ -0,0 +1,11 @@
+// Select the button
+const btn = document.querySelector('#btnTheme');
+const header = document.querySelector('header');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');
+  header.classList.toggle('dark-header');
+
+})
