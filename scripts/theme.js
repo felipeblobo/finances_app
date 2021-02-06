7d1a60e3899ed76e89126be1092d0c0004462a5e
Felipe Lobo <felipeblobo@gmail.com>
2021-02-06 11:24:55 -0300

    ajustes na organização do projeto

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,26 +1,27 @@
 // Select the button
 const btn = document.querySelector('.btnTheme');
 const header = document.querySelector('header');
-const imgMode = document.querySelector('#imgMode');
+const dayNight = document.querySelector('#dayNight');
 const footer = document.querySelector('footer');
 const modal = document.querySelector('.modal');
 const formTitle = document.querySelector('.h2');
 
 // Listen for a click on the button
 const darkMode = btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body, header and footer
+
+  // Then toggle (add/remove) classes to change theme
   document.body.classList.toggle('dark-theme');
   header.classList.toggle('dark-header');
-  footer.classList.toggle('darkfooter'); 
-  modal.classList.toggle('darkmodal');
+  footer.classList.toggle('dark-footer'); 
+  modal.classList.toggle('dark-modal');
   formTitle.classList.toggle('darkTitle');
 
   //dark or light symbol
-  imgMode.classList.toggle('fa-moon');
-  imgMode.classList.toggle('fa-sun');
+  dayNight.classList.toggle('fa-moon');
+  dayNight.classList.toggle('fa-sun');
   
-  imgMode.classList.toggle('whiteMoon');
-  imgMode.classList.toggle('yellowSun');
+  dayNight.classList.toggle('whiteMoon');
+  dayNight.classList.toggle('yellowSun');
 });
 
 let dark = localStorage.getItem('darkMode');
