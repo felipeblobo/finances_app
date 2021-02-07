108ddef7b53bab83a9043540d1d6ecf3afba1e80
Felipe Lobo <felipeblobo@gmail.com>
2021-02-07 20:29:14 -0300

    atualizando

--- scripts/script.js
+++ scripts/script.js
@@ -7,47 +7,43 @@ const Modal = {
   },
 };
 
-
 const changeColorCard = {
-  
   colorCard() {
-  const cardTotal = document.querySelector('.card.total');
-  
-  if (Transaction.total() < 0) {
-  cardTotal.classList.add('redCard') 
-  
-  } else {
-  cardTotal.classList.remove('redCard')
-  }
-  } }
+    const cardTotal = document.querySelector(".card.total");
+
+    if (Transaction.total() < 0) {
+      cardTotal.classList.add("redCard");
+    } else {
+      cardTotal.classList.remove("redCard");
+    }
+  },
+};
 
 //Armazenamento
 
 const Storage = {
-
   get() {
-    return JSON.parse(localStorage.getItem('devtransaction')) || [];
+    return JSON.parse(localStorage.getItem("devtransaction")) || [];
   },
 
   set(transactions) {
     localStorage.setItem("devtransaction", JSON.stringify(transactions));
-  }
+  },
 };
 
 //Global values
 
 const Transaction = {
   all: Storage.get(),
- 
-  
+
   add(transaction) {
     Transaction.all.push(transaction);
     App.reload();
   },
 
   remove(index) {
-      Transaction.all.splice(index, 1)
-      App.reload();
+    Transaction.all.splice(index, 1);
+    App.reload();
   },
 
   incomes() {
@@ -84,8 +80,8 @@ const DOM = {
     const tr = document.createElement("tr");
     tr.dataset = index;
     tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
-    
-    DOM.transactionsContainer.appendChild(tr);  
+
+    DOM.transactionsContainer.appendChild(tr);
   },
 
   innerHTMLTransaction(transaction, index) {
@@ -119,22 +115,19 @@ const DOM = {
   },
 
   clearTransactions() {
-      DOM.transactionsContainer.innerHTML = "";
-  }
+    DOM.transactionsContainer.innerHTML = "";
+  },
 };
 
 const Utilities = {
-
   formatDate(date) {
-    const splittedDate = date.split('-')
-    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
   },
 
   formatAmount(value) {
-
-
-    value = Number(value) * 100
-    return Math.round(value)
+    value = Number(value) * 100;
+    return Math.round(value);
   },
 
   formatCurrency(value) {
@@ -155,89 +148,108 @@ const Utilities = {
 //Prepare to show data (resumo) about transactions
 
 const Form = {
-    description: document.querySelector('input#description'),
-    amount: document.querySelector('input#amount'),
-    date: document.querySelector('input#date'),
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
 
-    getValues() {
-        return {
-            description: Form.description.value,
-            amount: Form.amount.value,
-            date: Form.date.value
-        }
-    },
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
 
-    validateFields() {
-      const {description, amount, date} = Form.getValues()
-      
-      if(description.trim() === '' || amount.trim() === '' || date.trim() === '') {
-        throw new Error('Por favor, preencha todos os campos.')
-      }
-    },
-    
-    formatValues() {
-      let {description, amount, date} = Form.getValues()
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
 
-      amount = Utilities.formatAmount(amount)
-
-      date = Utilities.formatDate(date)
-
-      return {
-        description,
-        amount,
-        date
-      }
-
-    },  
-
-    saveTransaction(transaction) {
-      Transaction.add(transaction)
-    },
-
-    clearFields() {
-      Form.description.value = ""
-      Form.amount.value = ""
-      Form.date.value = ""
-    },
-    
-    submit(event) {
-        event.preventDefault()
-
-        try {
-        
-         Form.validateFields() //valida os campos
-         const transaction = Form.formatValues() //formata os valores       
-         Form.saveTransaction(transaction)  //salva a transação
-         Form.clearFields()         //limpa os campos do form
-         Modal.openClose()            //fecha o modal
-         
-
-        } catch (error) {
-          alert(error.message)
-        }
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos.");
     }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utilities.formatAmount(amount);
+
+    date = Utilities.formatDate(date);
+   
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  saveTransaction(transaction) {
+    Transaction.add(transaction);
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields(); //valida os campos
+      const transaction = Form.formatValues(); //formata os valores
+      Form.saveTransaction(transaction); //salva a transação
+      Form.clearFields(); //limpa os campos do form
+      Modal.openClose(); //fecha o modal
+    } catch (error) {
+      alert(error.message);
+    }
+  },
 };
 
-
 const App = {
-  
   init() {
     Transaction.all.forEach((transaction, index) => {
-        DOM.addTransaction(transaction, index);
-        
-    })
-      
-    DOM.updateBalance()
+      DOM.addTransaction(transaction, index);
+    });
+
+    DOM.updateBalance();
     changeColorCard.colorCard();
-    Storage.set(Transaction.all)
+    Storage.set(Transaction.all);
   },
 
   reload() {
-      DOM.clearTransactions();
-      App.init();
+    DOM.clearTransactions();
+    App.init();
   },
 };
 
-
 App.init();
 
+
+//searching data between date
+
+// }
+
+// const searchDate = {
+//   firstDate: document.querySelector("input#firstDate").value.split('-'),
+//   lastDate: document.querySelector("input#lastDate").value.split('-'),
+
+//   search() {
+//     for (let i = 0; i < Transaction.all.length; i++) {
+//       if (Transaction.all[i].date < lastDate) {
+//         console.log("oi");
+//       } else {
+//         console.log('olá')
+//       }
+//     }
+//   },
+// };
+
+
+// searchDate.search();
