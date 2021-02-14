b21b882cfa1fb36bbc20a475a4234f4cd98c8796
Felipe Lobo <felipeblobo@gmail.com>
2021-02-13 23:28:15 -0300

    adequando botões

--- scripts/theme.js
+++ scripts/theme.js
@@ -5,6 +5,17 @@ const dayNight = document.querySelector('#dayNight');
 const footer = document.querySelector('footer');
 const modal = document.querySelector('.modal');
 const formTitle = document.querySelector('.h2');
+const darkModalOverlay = document.querySelector('.modal-overlay');
+const cancelButton = document.querySelector('#cancelButton');
+
+
+//sumir o texto de cancelar 
+cancelButton.addEventListener('mouseover', () => {
+  cancelButton.innerHTML = '';
+});
+cancelButton.addEventListener('mouseout', () => {
+  cancelButton.innerHTML = 'Cancelar';
+})
 
 // Listen for a click on the button
 const darkMode = btn.addEventListener('click', function() {
@@ -13,8 +24,12 @@ const darkMode = btn.addEventListener('click', function() {
   document.body.classList.toggle('dark-theme');
   header.classList.toggle('dark-header');
   footer.classList.toggle('dark-footer'); 
+  darkModalOverlay.classList.toggle('dark-modal-overlay');
   modal.classList.toggle('dark-modal');
+  modal.classList.toggle('dark-modal::before');
+  modal.classList.toggle('dark-modal::after');
   formTitle.classList.toggle('darkTitle');
+ 
 
   //dark or light symbol
   dayNight.classList.toggle('fa-moon');
